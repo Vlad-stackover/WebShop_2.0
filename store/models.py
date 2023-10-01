2b20ba02c2a82b692a5c3627aa7092861c985cab
Vlad <strixus480@gmail.com>
2023-10-01 22:24:06 +0200

    v4.6
    Migrate pricve in models

--- store/models.py
+++ store/models.py
@@ -25,7 +25,7 @@ class Category(models.Model):
 
 class Product(models.Model):
 	name = models.CharField(max_length=200)
-	price = models.FloatField()
+	price = models.DecimalField(max_digits=7, decimal_places=2)
 	digital = models.BooleanField(default=False,null=True, blank=True)
 	image = models.ImageField(null=True, blank=True)
 	category = models.CharField(max_length=100, default='uncategorize')
